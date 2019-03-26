0b66cd07baab03375d5672feeebe512149262f3d
Vincent Guidry <vincent.guidry@tinroofsoftware.com>
2019-03-25 22:10:58 -0400

    wip

--- config/initializers/hyperstack.rb
+++ config/initializers/hyperstack.rb
@@ -1,10 +1,9 @@
 # config/initializers/hyperstack.rb
 # If you are not using ActionCable, see http://hyperstack.orgs/docs/models/configuring-transport/
 Hyperstack.configuration do |config|
-  config.transport = :action_cable
-  config.prerendering = :off # or :on
+  # config.prerendering = :off # or :on
   config.cancel_import 'react/react-source-browser' # bring your own React and ReactRouter via Yarn/Webpacker
-  config.import 'hyperstack/component/jquery', client_only: true # remove this line if you don't need jquery
+  # config.import 'hyperstack/component/jquery', client_only: true # remove this line if you don't need jquery
   config.import 'hyperstack/hotloader', client_only: true if Rails.env.development?
 end
 
